648d2cf39ed108a3684fa428960cf0704cbecc47
Mastermeint <surfdude.jr@gmail.com>
2017-03-17 11:46:35 +0100

    almost working convertion from networkx to dig struc

--- dataStrucDIG/python/dig.py
+++ dataStrucDIG/python/dig.py
@@ -14,6 +14,20 @@ import networkx as nx
 
 G=nx.Graph()
 
+# source for computing fast prime list
+# http://stackoverflow.com/questions/16004407/a-fast-prime-number-sieve-in-python
+def sieve_for_primes_to(n):
+    size = n//2
+    sieve = [1]*size
+    limit = int(n**0.5)
+    for i in range(1,limit):
+        if sieve[i]:
+            val = 2*i+1
+            tmp = ((size-1) - i)//val 
+            sieve[i+val::val] = [0]*tmp
+    return [2] + [i*2+1 for i, v in enumerate(sieve) if v and i>0]
+
+
 # G.add_node(1)
 # G.add_nodes_from([2,3])
 
@@ -21,8 +35,32 @@ G=nx.Graph()
 # G.add_nodes_from(H)
 # G.add_node(H)
 
+
+# every DIG vertex is represented as a tuple:
+#           (offset, jumpsize, stepNumber)
+# TODO: Implement offset and stepnumber
 def convert_to_dig( G ):
     "converts a graph to a dotted interval graph"
     listOfNodes = G.nodes()
     listOfEdges = G.edges()
 
+    N = length(listOfNodes)
+    NLisPrime = sieve_for_primes_to(N)
+    
+    # initialize DIG vertex
+    for it in range(N):
+        DIG = []
+        p0 = NLisPrime(it)
+        jumpsize = p0
+        offset = 0
+        stepNumber = 0
+
+        for it2 in range(it):
+            if (G.has_edge(it,it2) || G.has_edge(it2,it)):
+                pm = NLisPrime(it2)
+                jumpsize = jumpsize * pm
+                #offset = offset * pm
+        #offset = (offset + 1) * p0
+
+        DIG.append((offset, jumpsize, stepNumber))
+    return DIG
