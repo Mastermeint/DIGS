e25119ead66d694995329f117fcabe42368a5eef
Mastermeint <surfdude.jr@gmail.com>
2017-05-20 16:37:48 +0200

    added functions complete bipartite graph in DIGk and create a random DIG graph

--- dataStrucDIG/python/dig.py
+++ dataStrucDIG/python/dig.py
@@ -1,14 +1,11 @@
 import networkx as nx
-import itertools
+import matplotlib.pyplot as plt
+
+from itertools import combinations 
 from fractions import gcd
+from random import randint
 
 #vertex: offset, jumpsize, steps
-class vertex:
-    def __init__(self, offset = 0, jumpsize = 0, steps = 0):
-        self.offset = offset
-        self.jumpsize = jumpsize
-        self.steps = steps
-
 class DIGd:
     def __init__(self, vertices = [], dtype = None):
         try:
@@ -20,7 +17,7 @@ class DIGd:
         
     def dig_to_networkX(self):
         G = nx.Graph()
-        for vertex1, vertex2 in itertools.combinations(self.vertices, 2):
+        for vertex1, vertex2 in combinations(self.vertices, 2):
             G.add_node(vertex1)
             G.add_node(vertex2)
             if (((vertex1[0] <= vertex2[0] and 
@@ -38,7 +35,7 @@ class DIGd:
 # create a cycle in DIG2 of length n
 # assume a cycle is always of length larger than 0
 def dig_cycle(n):
-    D = DIGd([((0,1,1))], "cycle")
+    D = DIGd([(0,1,1)], "cycle")
     if n == 1: return D
     for num_vertex in range(n-2):
         D.max_jump = 2
@@ -46,15 +43,37 @@ def dig_cycle(n):
     D.vertices.append((n-2,1,1))
     return D
 
-#cylce1:
-#[(0, 1, 1)]
+# create a complete bipartite graph of size k in DIGk, k>0
+def dig_complete_bipartite(k):
+    D = DIGd([], "cbipartite")
+    D.max_jump = k
+    for i in range(k):
+        D.vertices.append((i,k,k-1))
+    for j in range(k):
+        D.vertices.append((j*k,1,k-1))
+    return D
 
-#cycle2:
-#[(0,1,1), (0,1,1)]
+def plot_bipartite_graph(G):
+   # Separate by group
+   l, r = nx.bipartite.sets(G)
+   pos = {}
+   
+   # Update position for node from each group
+   pos.update((node, (1, index)) for index, node in enumerate(l))
+   pos.update((node, (2, index)) for index, node in enumerate(r))
+   
+   nx.draw(G, pos=pos)
+   plt.show()
 
-#cycle3:
-#[(0,1,1), (0,2,1), (1,1,1)]
+# create a random dig within interval tup_int_val 
+# with maximal jump max_jump containing num_vertices vertices
+def create_random_dig(tup_int_val, max_jump, num_vertices):
+    vertices = []
+    for i in range(num_vertices):
+        offset = randint(tup_int_val[0], tup_int_val[1])
+        jump = randint(0, min(max_jump,tup_int_val[1]-offset))
+        steps = randint(0, (tup_int_val[1]-offset)//max_jump)
+        vertices.append((offset, jump, steps))
+    return DIGd(vertices)
 
-#cycle4:
-#[(0,1,1), (0,2,1), (1,2,1), (2,1,1)]
-    
+
